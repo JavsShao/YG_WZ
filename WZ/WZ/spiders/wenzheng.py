215cf1f36f4375262f83df2f3205547a70af69ac
hamdi <javs_shao@163.com>
2018-12-28 16:43:18 +0800

    初始字段

--- WZ/WZ/spiders/wenzheng.py
+++ WZ/WZ/spiders/wenzheng.py
@@ -6,6 +6,9 @@ class WenzhengSpider(scrapy.Spider):
     name = 'wenzheng'
     allowed_domains = ['wz.sun0769.com']
     start_urls = ['http://wz.sun0769.com/']
+    url = 'http://wz.sun0769.com/index.php/question/questionType?type=4&page='
+    offset = 0
+    start_urls = [url + str(offset)]
 
     def parse(self, response):
         pass
