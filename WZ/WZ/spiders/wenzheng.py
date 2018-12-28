416cafdb26594b87e336250188d93526009238cb
hamdi <javs_shao@163.com>
2018-12-28 17:05:10 +0800

    获取所有页面的链接(符合要求)

--- WZ/WZ/spiders/wenzheng.py
+++ WZ/WZ/spiders/wenzheng.py
@@ -9,5 +9,19 @@ class WenzhengSpider(scrapy.Spider):
     offset = 0
     start_urls = [url + str(offset)]
 
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name=None, **kwargs)
+        self.parse_item = None
+
     def parse(self, response):
-        pass
+        # 取出每个页面里面帖子链接列表
+        links = response.xpath('//*[@id="morelist"]/div/table[2]/tbody/tr/td/table/tbody/tr/td[2]/a[2]/@href').extract()
+
+        # 迭代发送每个帖子的请求, 调用parse_item方法处理
+        for link in links:
+            yield scrapy.Request(link, callback=self.parse_item)
+        # 设置页码终止条件, 并且每次发送的新页面请求调用parse方法处理
+        if self.offset <= 71130:
+            self.offset += 30
+            yield scrapy.Request(self.url + str(self.offset), callback=self.parse)
+
