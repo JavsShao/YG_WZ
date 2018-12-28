a1f2a56e5d9a373bcae5bfbb819e4823a07b446d
hamdi <javs_shao@163.com>
2018-12-28 16:41:50 +0800

    items_创建字段

--- WZ/WZ/items.py
+++ WZ/WZ/items.py
@@ -11,4 +11,7 @@ import scrapy
 class WzItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    number = scrapy.Field()
+    content = scrapy.Field()
+    url = scrapy.Field()
