53840cca019133725c6386cf446382cbaa507b73
hamdi <javs_shao@163.com>
2018-12-28 16:43:48 +0800

    初始化值

--- WZ/WZ/spiders/wenzheng.py
+++ WZ/WZ/spiders/wenzheng.py
@@ -5,7 +5,6 @@ import scrapy
 class WenzhengSpider(scrapy.Spider):
     name = 'wenzheng'
     allowed_domains = ['wz.sun0769.com']
-    start_urls = ['http://wz.sun0769.com/']
     url = 'http://wz.sun0769.com/index.php/question/questionType?type=4&page='
     offset = 0
     start_urls = [url + str(offset)]
