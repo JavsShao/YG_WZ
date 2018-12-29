aedd6d858521e6ac8fab3dfea7a57aec0b4aabed
hamdi <javs_shao@163.com>
2018-12-29 13:59:51 +0800

    管道文件_spider_closed

--- WZ/WZ/pipelines.py
+++ WZ/WZ/pipelines.py
@@ -15,7 +15,11 @@ class WzPipeline(object):
         创建一个只写文件,指定文本编码格式为utf-8
         '''
         self.filename = codecs.open('wenzheng.json', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
         content = json.dumps(dict(item), ensure_ascii=False) + '\n'
         self.filename.write(content)
         return item
+
+    def spider_closed(self, spider):
+        self.filename.close()
