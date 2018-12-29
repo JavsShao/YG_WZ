600477e62a6b1b5a60da09b3d5000c260f7fec43
hamdi <javs_shao@163.com>
2018-12-29 13:56:42 +0800

    管道文件_初始化_创建一个只写文件

--- WZ/WZ/pipelines.py
+++ WZ/WZ/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
 
 
 class WzPipeline(object):
+
+    def __init__(self):
+        '''
+        创建一个只写文件,指定文本编码格式为utf-8
+        '''
+        self.filename = codecs.open('wenzheng.json', 'w', encoding='utf-8')
     def process_item(self, item, spider):
         return item
