6d32907a56e66d315c1268f64aaa2138531e5a92
hamdi <javs_shao@163.com>
2018-12-29 13:51:41 +0800

    代码修正_2

--- WZ/WZ/spiders/wenzheng.py
+++ WZ/WZ/spiders/wenzheng.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from WZ.items import WzItem
 
 
-class WenzhengSpider(scrapy.Spider):
+class WenzhengSpider(CrawlSpider):
     name = 'wenzheng'
-    allowed_domains = ['wz.sun0769.com']
+    allowed_domains = ['wz.sun0769.com/']
     url = 'http://wz.sun0769.com/index.php/question/questionType?type=4&page='
     offset = 0
     start_urls = [url + str(offset)]
 
-    def parse(self, response):
-        # 取出每个页面里面帖子链接列表
-        links = response.xpath('//*[@id="morelist"]/div/table[2]/tbody/tr/td/table/tbody/tr/td[2]/a[2]/@href').extract()
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
 
-        # 迭代发送每个帖子的请求, 调用parse_item方法处理
+    def parse(self, response):
+        # 取出每个页面里帖子链接列表
+        links = response.xpath("//div[@class='greyframe']/table//td/a[@class='news14']/@href").extract()
+        # 迭代发送每个帖子的请求，调用parse_item方法处理
         for link in links:
             yield scrapy.Request(link, callback=self.parse_item)
-        # 设置页码终止条件, 并且每次发送的新页面请求调用parse方法处理
+        # 设置页码终止条件，并且每次发送新的页面请求调用parse方法处理
         if self.offset <= 71130:
             self.offset += 30
             yield scrapy.Request(self.url + str(self.offset), callback=self.parse)
 
     def parse_item(self, response):
-        """
-        解析源码
-        :param response:
-        :return:
-        """
-        item = WzItem()
-
-
+        i = {}
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
